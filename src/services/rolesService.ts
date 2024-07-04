dc4f6aeb6efdcc8ea47f961a93970d0adc01a9ef
Shakhriyor <62033235+shakhriyor699@users.noreply.github.com>
2024-07-04 17:20:44 +0500

    Update rolesService.ts

--- src/services/rolesService.ts
+++ src/services/rolesService.ts
@@ -13,8 +13,8 @@ export const deleteRole = async (id: number) => {
   return res
 }
 
-export const createRole = async (data: IRoles) => {
-  const res = await axiosWithAuth.post<IRoles>('/roles/create', data)
+export const createRole = async (data: any) => {
+  const res = await axiosWithAuth.post('/roles/create', data)
   return res
 }
 
